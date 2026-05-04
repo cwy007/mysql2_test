301eae64a354b88c37ead60a44694eefc399da01
chanweiyan <chanweiyan007@gmail.com>
2026-05-04 10:52:37 +0800

    fix: SELECT * FROM customers

---
+++ index.js
@@ -0,0 +1,32 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'Cwy17824',
+  database: 'practice',
+})
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database:', err);
+    return;
+  }
+  console.log('Connected to the database.');
+});
+
+connection.query('SELECT * FROM customers', (err, results) => {
+  if (err) {
+    console.error('Error executing query:', err);
+    return;
+  }
+  console.log('Query results:', results);
+});
+
+connection.end((err) => {
+  if (err) {
+    console.error('Error closing the database connection:', err);
+    return;
+  }
+  console.log('Database connection closed.');
+});
